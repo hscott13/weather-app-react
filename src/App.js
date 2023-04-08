102dbda66b2b175bbfacc796e4231e45e0fe9670
hscott13 <hscott13@hotmail.co.uk>
2023-04-08 17:53:15 +0100

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
+
 import Search from "./Search";
 import Forecast from "./Forecast";
 
 function App() {
-  let key = `ca5af28648d86b7925348bb9fb85cd3a`;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`;
-
   return (
     <div className="App">
       <header className="App-header">
